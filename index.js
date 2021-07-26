53e335263bc71fca8fa6409281579b15ce60a349
Reid <rlj0713@gmail.com>
2021-07-25 21:01:12 -0400

    commit tree 938c643ee5dea51aed7909f0c1410105aa497b35

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let firstSentence = document.querySelector('p')
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+}); 
+
+function updateDOM() {
+    firstSentence.textContent = "This is really cool!"
+}
